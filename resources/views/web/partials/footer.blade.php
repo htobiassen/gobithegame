d5d3315c0f8b466fe4cbe74d48524ae4687e24aa
htobiassen <hst@metabit.dk>
2024-12-02 19:58:57 +0100

    footer

--- resources/views/web/partials/footer.blade.php
+++ resources/views/web/partials/footer.blade.php
@@ -3,12 +3,18 @@
 		<div class="row justify-content-center">
 			<div class="col-12 col-lg-4 mb-5 text-center text-md-start">
                 <a class="d-flex align-items-center text-decoration-none" href="#">
-                    <div class="fs-3 pe-4 text-tertiary fw-medium text-center text-md-start w-100 mb-3">
-                        ©2024 GOBI THE GAME
+                    <div class="fw-bold text-uppercase text-tertiary mb-3">
+                        $GOBI official channels
                     </div>
                     <a href="https://twitter.com/gobixyz" title="X" class="me-3 text-decoration-none text-tertiary" target="_blank">
-                        $GOBI <i class="fa-brands fa-x-twitter fs-5 text-tertiary"></i>
+                        <i class="fa-brands fa-x-twitter fs-5 text-tertiary"></i>
                     </a>
+                    <a href="https://t.me/GOBIversecommunity" title="X" class="me-3 text-decoration-none text-tertiary" target="_blank">
+                        <i class="fa-brands fa-telegram fs-5 text-tertiary"></i>
+                    </a>
+                    <div class="text-tertiary mt-3">
+                        <span class="fw-bold">CA:</span> CwdjgD54hgTQChspxHhirWLQWqy3EsxtndrcpLBqpump
+                    </div>
                 </a>
 			</div>
 
@@ -22,6 +28,9 @@
                 <a href="https://twitter.com/heintriss" title="X" class="me-3 text-decoration-none text-tertiary" target="_blank">
                     Heintriss <i class="fa-brands fa-x-twitter fs-5 text-tertiary"></i>
                 </a>
+                <div class="text-tertiary mt-3">
+                    ©2024 GOBI THE GAME
+                </div>
 			</div>
 		</div>
 	</div>
