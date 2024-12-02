e7b4c8c52a50bcfe7c421ec8eee7fa15a1018902
htobiassen <hst@metabit.dk>
2024-12-02 20:20:42 +0100

    fix

--- resources/views/web/partials/footer.blade.php
+++ resources/views/web/partials/footer.blade.php
@@ -2,20 +2,18 @@
 	<div class="container py-5">
 		<div class="row justify-content-center">
 			<div class="col-12 col-lg-4 mb-5 text-center text-md-start">
-                <a class="d-flex align-items-center text-decoration-none" href="#">
-                    <div class="fw-bold text-uppercase text-tertiary mb-3">
-                        $GOBI official channels
-                    </div>
-                    <a href="https://twitter.com/gobixyz" title="X" class="me-3 text-decoration-none text-tertiary" target="_blank">
-                        <i class="fa-brands fa-x-twitter fs-5 text-tertiary"></i>
-                    </a>
-                    <a href="https://t.me/GOBIversecommunity" title="Telegram" class="me-3 text-decoration-none text-tertiary" target="_blank">
-                        <i class="fa-brands fa-telegram fs-5 text-tertiary"></i>
-                    </a>
+                <div class="fw-bold text-uppercase text-tertiary mb-3">
+                    $GOBI official channels
+                </div>
+                <a href="https://twitter.com/gobixyz" title="X" class="me-3 text-decoration-none text-tertiary" target="_blank">
+                    <i class="fa-brands fa-x-twitter fs-5 text-tertiary"></i>
+                </a>
+                <a href="https://t.me/GOBIversecommunity" title="Telegram" class="me-3 text-decoration-none text-tertiary" target="_blank">
+                    <i class="fa-brands fa-telegram fs-5 text-tertiary"></i>
+                </a>
 
-                    <a href="https://dexscreener.com/solana/adarbxupgfh8sbwamigcptcux8b83tnu5erhhmmybfn9" title="X" class="d-block mt-3 text-decoration-none text-tertiary" target="_blank">
-                        Dexscreener
-                    </a>
+                <a href="https://dexscreener.com/solana/adarbxupgfh8sbwamigcptcux8b83tnu5erhhmmybfn9" title="X" class="d-block mt-3 text-decoration-none text-tertiary" target="_blank">
+                    Dexscreener
                 </a>
 			</div>
 
